88729085ba362be90207262a0c9613342b5f3e05
Kévin Ta <kevin.ta@edu.esiee.fr>
2015-07-05 09:53:20 +0200

    Fix scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,14 @@
-$(document).ready(function() {
-		$('.scrollTo').click( function() { // Au clic sur un élément
-			var page = $(this).attr('href'); // Page cible
-			var speed = 750; // Durée de l'animation (en ms)
-			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-			return false;
-		});
-	});
+$(document).ready(function () {
+     $('a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
+
+         var target = this.hash,
+             $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top - 50
+         }, 900, 'swing');
+     });
+ });
+ 
+ 
